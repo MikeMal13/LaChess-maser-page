c95524ab26dfa6a829d1d11f4589339cc36170e0
MikeMal13 <95088047+MikeMal13@users.noreply.github.com>
2023-05-31 23:54:11 +0300

    admin need help and need to finish useres

--- Pages/Settings/adminPage.aspx
+++ Pages/Settings/adminPage.aspx
@@ -15,45 +15,60 @@
     <br />
 
 &nbsp;<div id="nameDiv"></div>
-                <label>name:</label>
+                
+    <label>name:</label>
                 <br />
                 <input type="text" name="name" id="name" oninput="checkName()" />
                 <br />
-                <div id="password1Div"></div>
+    
+    <div id="password1Div"></div>
                 <label>password:</label>
                 <br />
                 <input type="password" name="password1" id="password1" oninput="checkPassword1()" />
-                <div id="password2Div"></div>
+    
+    <div id="password2Div"></div>
                 <label>confirm password:</label>
                 <br />
                 <input type="password" name="password2" id="password2" oninput="checkPassword2()" />
-                <div id="mailDiv"></div>
+    
+    <div id="mailDiv"></div>
                 <label>mail:</label>
                 <br />
                 <input type="text" name="mail" id="mail" oninput="checkMail()" />
                 <br />
                 <div id="radioDiv"></div>
-                <p>Choose your Role:</p>
+               
+    <p>Replace your gender ❤️:</p>
                 <br />
-                <input type="radio" name="Role" value="GM" id="GM">
+                <input type="radio" name="gender" value="male" id="male (trans)">
                 <label>GM</label><br>
-                <input type="radio" name="Role" value="Player" id="PLayer">
+                <input type="radio" name="gender" value="female" id="female (trans)">
                 <label>Player</label><br>
-                <input type="radio" name="Role" value="Both" id="Both">
+                <input type="radio" name="gender" value="Both" id="Both">
                 <label>Both</label>
-                <div id="checkBoxDiv"></div>
-                <p>choose version/s (one or more) :</p>
-                <input type="checkbox" name="version1" value="version1"id="version1">
-                <label>version1</label><br>
-                <input type="checkbox" name="version2" value="version2"id="version2">
-                <label>version2</label><br>
-                <input type="checkbox" name="version3" value="version3"id="version3">
-                <label>version3</label><br>
-                <input type="checkbox" name="version4" value="version4"id="version4">
-                <label>version4</label><br>
-                <input type="checkbox" name="version5" value="version5"id="version5">
-                <label>version5</label><br>
-                <div id="birthdayDiv"></div>
-                <label>Birthday:</label>
+    
+    <div id="checkBoxDiv"></div>
+    <p>choose favorit time controll:</p>
+                <input type="checkbox" name="time1" value="version1"id="version1">
+                <label>bullet  (0.5 - 1.5)</label><br>
+                <input type="checkbox" name="time2" value="version2"id="version2">
+                <label>blitz   (2 - 5)</label><br>
+                <input type="checkbox" name="time3" value="version3"id="version3">
+                <label>casual  (10 - 20)</label><br>
+                <input type="checkbox" name="time4" value="version4"id="version4">
+                <label>long (30 - 90)</label><br>
+                <input type="checkbox" name="time5" value="version5"id="version5">
+                <label>day (24 hours - 4 weeks)</label><br>
+                
+    <div id="birthdayDiv"></div>
+    <label>Birthday:</label>
                 <input type="date" name="birthday" id="birthday">
+
+                                <select name="livingArea" id="carFromSelect">
+                                   <option value="">------</option>
+                                    <option value="center">center</option>
+                                    <option value="south">south</option>
+                                    <option value="west">west</option>
+                                    <option value="east">east</option>
+                                </select>
 </asp:Content>
--- Pages/Settings/adminPage.aspx.cs
+++ Pages/Settings/adminPage.aspx.cs
@@ -12,6 +12,13 @@ namespace LaChess_maser_page.Pages.Settings
 {
     public partial class adminPage : System.Web.UI.Page
     {
+        public static string addIfNotNull(string dataRow, string data)
+        {
+            if (data != "")
+                return "," + dataRow + "='" + data + "'";
+            return "";
+        }
+
         string str;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -47,29 +54,35 @@ namespace LaChess_maser_page.Pages.Settings
 
             if (IsPostBack)
             {
+                int id = -1;
+                try { 
+                    id = int.Parse(Request.Form["id"]);
+                }
+                catch { } //id stays -1
+
+                String name = Request.Form["name"];
+                String password1 = Request.Form["password1"];
+                String mail = Request.Form["mail"];
+                String time1 = Request.Form["time1"];
+                String time2 = Request.Form["time2"];
+                String time3 = Request.Form["time3"];
+                String time4 = Request.Form["time4"];
+                String time5 = Request.Form["time5"];
+                String gender = Request.Form["gender"];
+                String livingArea = Request.Form["livingArea"];
+                String birthday = Request.Form["birthday"];
+
+
                 string btn = Request.Form["btn"];
                 if (Request.Form["btn"] == "add")
                 {
-                    String name = Request.Form["name"];
-                    String password1 = Request.Form["password1"];
-                    String mail = Request.Form["mail"];
-                    String Role = Request.Form["Role"];
-                    String version1 = Request.Form["version1"];
-                    String version2 = Request.Form["version2"];
-                    String version3 = Request.Form["version3"];
-                    String version4 = Request.Form["version4"];
-                    String version5 = Request.Form["version5"];
-                    String birthday = Request.Form["birthday"];
-                    s = "insert into Users(name,password1,mail,Role,version1,version2,version3,version4,version5,birthday) values('" + name + "','" + password1 + "','" + mail + "','" + Role + "','" + version1 + "','" + version2 + "','" + version3 + "','" + version4 + "','" + version5 + "','" + birthday + "')";
+                    s = "insert into Users(name,password,mail,time1,time2,time3,time4,time5,livingArea,birthday) values('" + name + "','" + password1 + "','" + mail + "','" + time1 + "','" + time2 + "','" + time3 + "','" + time4 + "','" + time5 + "','" + livingArea + "','" + birthday + "')";
                     da = new SqlDataAdapter(s, con);
                     ds = new DataSet();
                     da.Fill(ds);
                 }
                 else if (Request.Form["btn"] == "delete")
                 {
-
-                    int id = int.Parse(Request.Form["id"]);
-
                     s = "delete from Users where id=" + id + "";
                     da = new SqlDataAdapter(s, con);
                     ds = new DataSet();
@@ -78,20 +91,14 @@ namespace LaChess_maser_page.Pages.Settings
                 }
                 else if (Request.Form["btn"] == "update")
                 {
-
-                    String name = Request.Form["name"];
-                    String password1 = Request.Form["password1"];
-                    String mail = Request.Form["mail"];
-                    String Role = Request.Form["Role"];
-                    String version1 = Request.Form["version1"];
-                    String version2 = Request.Form["version2"];
-                    String version3 = Request.Form["version3"];
-                    String version4 = Request.Form["version4"];
-                    String version5 = Request.Form["version5"];
-                    String birthday = Request.Form["birthday"];
-
-                    int id = int.Parse(Request.Form["id"]);
-                    s = "update Users set name='" + name + "',password='" + password1 + "',mail='" + mail + "' where id= " + id + "";
+                    s = "update Users set" + (name != "" ? " name='" + name + "'": "") + 
+                        addIfNotNull("password", password1) + addIfNotNull("mail", mail) + 
+                        addIfNotNull("time1", time1) + addIfNotNull("time2", time2) + 
+                        addIfNotNull("time3", time3) + addIfNotNull("time4", time4)+
+                        addIfNotNull("time5", time5) + addIfNotNull("gender", gender)+
+                        addIfNotNull("livingArea", livingArea) + addIfNotNull("birthday", birthday)+
+                        "' where id= " + id + "";
+                    Console.WriteLine(s);
                     da = new SqlDataAdapter(s, con);
                     da.Fill(ds);
                 }
